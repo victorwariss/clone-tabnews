cf0ff38ed06d18ce8cab66320936af9dd21698ef
Victor Wariss <victor.wariss@outlook.com>
2025-08-20 16:07:24 -0300

    add `database.js` and `.env` intentionally files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result);
   response
     .status(200)
     .json({ mensagem: "Alunos do curso.dev são pessoas acima da media" });
