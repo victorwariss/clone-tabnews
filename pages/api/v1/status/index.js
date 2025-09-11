286036204d64df2ccdd872258c5efe94893245de
Victor Wariss <victor.wariss@outlook.com>
2025-09-11 17:01:31 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  response
-    .status(200)
-    .json({ mensagem: "Alunos do curso.dev são pessoas acima da media" });
+  const updatedAt = new Date().toISOString();
+  const version = await database.query(`SHOW server_version;`);
+  const maxConnections = await database.query(
+    `SELECT setting FROM PG_SETTINGS WHERE NAME = 'max_connections';`,
+  );
+  const activiesConnections = await database.query(
+    `SELECT COUNT(*) FROM pg_stat_activity;`,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].setting),
+        actives_connections: parseInt(activiesConnections.rows[0].count),
+      },
+    },
+  });
 }
 
 export default status;
